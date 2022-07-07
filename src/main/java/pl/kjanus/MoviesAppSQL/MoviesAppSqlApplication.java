67355bd783523cf46d8f1de573af64b4ba107df8
Katarzyna Janus <katarzyna.swiniarska@janusgi.com>
2022-07-07 17:41:16 +0200

    first commit

---
+++ src/main/java/pl/kjanus/MoviesAppSQL/MoviesAppSqlApplication.java
@@ -0,0 +1,13 @@
+package pl.kjanus.MoviesAppSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAppSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAppSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kjanus/MoviesAppSQL/MoviesAppSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kjanus.MoviesAppSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAppSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
